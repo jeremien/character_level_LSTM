061a0db2f06dccea46d06016040d443e1df5fff7
jeremien <jeremie.nuel@gmail.com>
2019-05-14 18:38:01 +0200

    train fr

--- process_training.py
+++ process_training.py
@@ -13,13 +13,13 @@ device = tools.device()
 if 'net' in locals():
     del net
 
-net = CharRNN(chars, n_hidden=512, n_layers=4)
+net = CharRNN(chars, n_hidden=512, n_layers=6)
 
 print(len(text))
 print(net)
 
-n_seqs, n_steps = 256, 150
-model_name = 'rnn_100_epoch_fr_256_150_6.net'
+n_seqs, n_steps = 256, 120
+model_name = 'rnn_100_epoch_fr_256_120_6.net'
 
 try:
     train(net, encoded, epochs=100, n_seqs=n_seqs, n_steps=n_steps, lr=0.001, cuda=device, print_every=100)
