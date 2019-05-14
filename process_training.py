be34b27cbf464e2a467addb6a6fb8a3f9d1bc5c2
jeremien <jeremie.nuel@gmail.com>
2019-05-14 10:51:31 +0200

    process fr

--- process_training.py
+++ process_training.py
@@ -22,7 +22,7 @@ n_seqs, n_steps = 256, 150
 model_name = 'rnn_1_epoch.net'
 
 try:
-    train(net, encoded, epochs=50, n_seqs=n_seqs, n_steps=n_steps, lr=0.001, cuda=device, print_every=10)
+    train(net, encoded, epochs=100, n_seqs=n_seqs, n_steps=n_steps, lr=0.001, cuda=device, print_every=10)
    
     tools.save_model(net, model_name)
 
