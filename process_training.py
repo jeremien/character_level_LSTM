1bd22c93939f825a06fe8c0b199949281742c35e
jeremien <jeremie.nuel@gmail.com>
2019-05-14 10:51:14 +0200

    process fr

--- process_training.py
+++ process_training.py
@@ -13,12 +13,12 @@ device = tools.device()
 if 'net' in locals():
     del net
 
-net = CharRNN(chars, n_hidden=512, n_layers=2)
+net = CharRNN(chars, n_hidden=512, n_layers=4)
 
 print(len(text))
 print(net)
 
-n_seqs, n_steps = 128, 100
+n_seqs, n_steps = 256, 150
 model_name = 'rnn_1_epoch.net'
 
 try:
