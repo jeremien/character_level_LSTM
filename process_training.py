b33483ba6868a61538d28208df964cb8c613f9db
jeremien <jeremie.nuel@gmail.com>
2019-05-20 12:14:59 +0200

    training

--- process_training.py
+++ process_training.py
@@ -13,16 +13,16 @@ device = tools.device()
 if 'net' in locals():
     del net
 
-net = CharRNN(chars, n_hidden=512, n_layers=2, drop_prob=0.8)
+net = CharRNN(chars, n_hidden=512, n_layers=4, drop_prob=0.7)
 
 print(len(text))
 print(net)
 
 n_seqs, n_steps = 256, 200
-model_name = 'rnn_100_epoch_fr_256_250_2_0.8.net'
+model_name = 'rnn_100_epoch_fr_256_200_4_0.7.net'
 
 try:
-    train(net, encoded, epochs=50, n_seqs=n_seqs, n_steps=n_steps, lr=0.001, cuda=device, print_every=100)
+    train(net, encoded, epochs=100, n_seqs=n_seqs, n_steps=n_steps, lr=0.001, cuda=device, print_every=100)
     tools.save_model(net, model_name)
 
 except KeyboardInterrupt:
