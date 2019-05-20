27befe746dbca77b7c1f1d6d4b86e4818c92adfb
jeremien <jeremie.nuel@gmail.com>
2019-05-20 10:16:47 +0200

    training

--- process_training.py
+++ process_training.py
@@ -18,8 +18,8 @@ net = CharRNN(chars, n_hidden=512, n_layers=3, drop_prob=0.8)
 print(len(text))
 print(net)
 
-n_seqs, n_steps = 256, 150
-model_name = 'rnn_100_epoch_fr_256_150_3_0.8.net'
+n_seqs, n_steps = 256, 250
+model_name = 'rnn_100_epoch_fr_256_250_3_0.8.net'
 
 try:
     train(net, encoded, epochs=50, n_seqs=n_seqs, n_steps=n_steps, lr=0.001, cuda=device, print_every=100)
