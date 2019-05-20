57ed24867188611d1c6d1afe5b27b9266fe8b787
jeremien <jeremie.nuel@gmail.com>
2019-05-20 19:55:00 +0200

    config

--- process_training.py
+++ process_training.py
@@ -5,7 +5,13 @@ from model import *
 from train import *
 
 def main():
-    if len(sys.argv) > 1 :
+    if len(sys.argv) > 4 :
+
+        model_name = sys.argv[1]
+        epochs = sys.argv[2]
+        layers = sys.argv[3]
+        dropout = sys.argv[4]
+
         tools = Helpers()
 
         text, chars = tools.chars()
@@ -16,16 +22,15 @@ def main():
         if 'net' in locals():
             del net
 
-        net = CharRNN(chars, n_hidden=512, n_layers=3, drop_prob=0.7)
+        net = CharRNN(chars, n_hidden=512, n_layers=layers, drop_prob=dropout)
 
         print(len(text))
         print(net)
 
         n_seqs, n_steps = 256, 180
-        model_name = sys.argv[1]
 
         try:
-            train(net, encoded, epochs=100, n_seqs=n_seqs, n_steps=n_steps, lr=0.001, cuda=device, print_every=100)
+            train(net, encoded, epochs=epochs, n_seqs=n_seqs, n_steps=n_steps, lr=0.001, cuda=device, print_every=100)
             tools.save_model(net, model_name)
             print("model saved as {}".format(model_name))
 
@@ -33,7 +38,7 @@ def main():
             tools.save_model(net, model_name)
             print("model saved as {}".format(model_name))
     else: 
-        print("Usage : python process_training.py rnn_100_epoch_fr_256_200_2_0.7.net")
+        print("Usage : python process_training.py rnn_100_epoch_fr_256_200_2_0.7.net 100 3 0.7")
 
 if __name__ == "__main__":
     main()
